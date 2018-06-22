c57237bbf0d8cf3aa6d3c78ae95ba354c6f4b73c
matt <matt.rea@gmail.com>
2018-06-21 20:48:13 -0500

    cleaned up a few styles, tweaked headline onscroll settings

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ new WOW().init();
 window.onscroll = function(event){
   let heroText = document.getElementById('hero-text');
   let topHeight = document.getElementById('header').offsetHeight;
-  heroText.style.top = (topHeight - document.body.scrollTop * 2) + 'px';
+  heroText.style.top = (topHeight - document.body.scrollTop * 1.25) + 'px';
 };
 
 let skillToggle = document.getElementsByClassName('skill-toggle');
